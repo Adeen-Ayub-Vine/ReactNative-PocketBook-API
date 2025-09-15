04046abd5b4f8926a534a853ce05788f7e417c68
Adeen Ayub Vine <133694221+Adeen-Ayub-Vine@users.noreply.github.com>
2025-09-15 15:32:40 +0500

    Vercel changes

--- src/server.js
+++ src/server.js
@@ -2,21 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-
 import transactionsRoute from "./routes/TransactionRoutes.js";
-//import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") job.start();
-
 // middleware
 app.use(rateLimiter);
 app.use(express.json());
 
-const PORT = process.env.PORT || 5001;
+// const PORT = process.env.PORT || 5001;
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "ok" });
@@ -24,8 +20,10 @@ app.get("/api/health", (req, res) => {
 
 app.use("/api/transactions", transactionsRoute);
 
-initDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Server is up and running on PORT:", PORT);
-  });
-});
+// initDB().then(() => {
+//   app.listen(PORT, () => {
+//     console.log("Server is up and running on PORT:", PORT);
+//   });
+// });
+
+initDB();
